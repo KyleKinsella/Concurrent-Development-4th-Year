0bf32475880c91a12f58ab921cd042e449a6672a
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-18 10:40:24 +0100

    I have got the reusable barrier to workgit add .git add .

---
+++ Concurrent_Labs/Reusable-Barrier/barrier.go
@@ -0,0 +1,78 @@
+//Barrier.go Template Code
+//Copyright (C) 2024 Dr. Joseph Kehoe
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+//--------------------------------------------
+// Author: Joseph Kehoe (Joseph.Kehoe@setu.ie)
+// Created on 30/9/2024
+// Modified by: Kyle Kinsella - C00273146
+// Issues:
+// The barrier is not implemented! / I have now implemented a barrier with a mutex and semaphores added to the function called "doStuff"
+//--------------------------------------------
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+	"golang.org/x/sync/semaphore"
+)
+
+
+// Place a barrier in this function --use Mutex's and Semaphores
+func doStuff(goNum int, wg *sync.WaitGroup, m *sync.Mutex, sem *semaphore.Weighted, ctx context.Context) bool {//, sem *semaphore.Weighted, ctx context.Context, max int) bool {
+
+	defer wg.Done()
+
+	fmt.Println("Part A",goNum) // all part a's first then all of part b's 
+	for i:=0; i<2; i++ {
+		m.Lock()
+		sem.Acquire(ctx, 1)
+		goNum++
+		fmt.Println("Iteration: ", i)
+		time.Sleep(time.Second)
+		m.Unlock()
+		sem.Release(1)
+	}
+	fmt.Println("PartB:", goNum)
+	return true
+}
+
+
+func main() {	
+	// here i get the current local time 
+	now := time.Now()
+
+	// i print how long it takes to run the program at the end of the main function
+	defer func() {
+		fmt.Println(time.Since(now))
+	}()
+
+	totalRoutines := 2
+	var wg sync.WaitGroup
+	wg.Add(totalRoutines)
+	ctx := context.TODO()
+	var theLock sync.Mutex
+	sem := semaphore.NewWeighted(int64(totalRoutines))
+
+	for i := range totalRoutines {//create the go Routines here
+		// here i have added more parameters because i have updated my function called "doStuff"
+		go doStuff(i, &wg, &theLock, sem, ctx)
+	}
+	wg.Wait() //wait for everyone to finish before exiting		
+}
